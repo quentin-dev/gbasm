d27e5dbfa1be6be9022ac834dbd378e24255bcaa
Quentin Barbarat <quentin.barbarat@epita.fr>
2020-12-03 18:43:51 +0100

    interactions: add skelly man

--- src/interactions.asm
+++ src/interactions.asm
@@ -3,4 +3,6 @@ SECTION "List of all interactions", ROMX
 InteractionListStart::
 db $16, $09
 dw IAmNpcStrStart
+db $0B, $18
+dw SkellyManStrStart
 db 33
--- src/strings.asm
+++ src/strings.asm
@@ -4,16 +4,21 @@ charmap ",", $AC
 charmap "C", $C3
 charmap "H", $C8
 charmap "I", $C9
+charmap "M", $CD
 charmap "N", $CE
 charmap "P", $D0
+charmap "S", $D3
 charmap "W", $D7
 charmap "a", $E1
 charmap "d", $E4
 charmap "e", $E5
+charmap "k", $EB
 charmap "l", $EC
 charmap "m", $ED
+charmap "n", $EE
 charmap "o", $EF
 charmap "r", $F2
+charmap "y", $F9
 
 SECTION "Dialogue strings", ROMX
 
@@ -22,3 +27,6 @@ HelloWorldStrStart::
 
 IAmNpcStrStart::
     db "I am NPC!", 0
+
+SkellyManStrStart::
+    db "I am Skelly Man!", 0
